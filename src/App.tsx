d7330d7666476cb89ac60b6d6f4dd987a403aab9
Kseniia-Mekheda <ksenia.mekheda@gmail.com>
2025-12-19 13:05:02 +0200

    Triggering restart

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Dashboard from '~/pages/dashboard/Dashboard';
 import WishPage from '~/pages/wish-page/WishPage';
 import NotFoundPage from '~/pages/404/404';
 import URLs from '~/constants/routes'
-import './App.css'
+import './App.css' 
 
 function App() {
   return (
