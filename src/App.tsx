c8782c0d3c12f031ce35ebd424604aa6354dad2b
Kseniia-Mekheda <ksenia.mekheda@gmail.com>
2025-12-19 12:18:14 +0200

    Changed router to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from '~/pages/dashboard/Dashboard';
 import WishPage from '~/pages/wish-page/WishPage';
 import NotFoundPage from '~/pages/404/404';
 import URLs from '~/constants/routes'
 import './App.css'
 
-const basename = import.meta.env.BASE_URL;
-
 function App() {
   return (
     <>
-      <Router basename={basename}>
+      <Router>
         <Routes>
           <Route path={URLs.pages.dashboard} element={<Dashboard />} />
           <Route path={URLs.pages.wishPage} element={<WishPage />} />
